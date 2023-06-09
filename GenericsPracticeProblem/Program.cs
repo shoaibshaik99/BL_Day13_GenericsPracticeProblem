67a8394f366048400382e4f6f11bf185ed0ee14c
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-09 09:48:56 +0530

    [Shoaib] First sync of local and remote repos

---
+++ GenericsPracticeProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding Max using Generics");
+        }
+    }
+}
