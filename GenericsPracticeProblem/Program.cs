1e5e57dfbac45a6a757d2d649fb4b39cdb101968
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-09 09:52:48 +0530

    [Shoaib] Feature to find maximum of 3 integers

--- GenericsPracticeProblem/Program.cs
+++ GenericsPracticeProblem/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        static int FindMax(int a, int b, int c)
+        {
+            int max = a;
+            if (b.CompareTo(max) > 0)
+            {
+                max = b;
+            }
+            if (c.CompareTo(max) > 0)
+            {
+                max = c;
+            }
+            return max;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Finding Max using Generics");
+            Console.WriteLine("Finding Maximum using Generics");
+
+            // Test cases
+            int result1 = FindMax(15, 10, 5);
+            Console.WriteLine("Test case 1: Max number at first position: " + result1);
+
+            int result2 = FindMax(5, 15, 10);
+            Console.WriteLine("Test case 2: Max number at second position: " + result2);
+
+            int result3 = FindMax(10, 5, 15);
+            Console.WriteLine("Test case 3: Max number at third position: " + result3);
         }
     }
 }
